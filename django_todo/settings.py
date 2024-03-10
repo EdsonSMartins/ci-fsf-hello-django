2aa6ba4a707bb91095efeb41534496ab4966e9c9
EdsonSMartins <smartins.edson@gmail.com>
2024-03-10 22:43:20 +0000

    set up env variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = ['8000-edsonsmarti-cifsfhellod-2rau7hgr5cn.ws-eu108.gitpod.io']
 
 
 # Application definition
